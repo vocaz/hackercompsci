3ea8b42d8287cec6705ec1a18b521fcfce25e51a
gabe <100031278+vocaz@users.noreply.github.com>
2024-11-08 10:31:39 +0800

    creation of own main menu screen

--- main_menu.gd
+++ main_menu.gd
@@ -1,6 +1,17 @@
 extends Node2D
 
+var websocket_url := "ws://127.0.0.1:9876"
 
+var socket := WebSocketPeer.new()
+
+var connected := false
+func send(message: String):
+	#socket.put_packet(message.to_utf8_buffer())
+	socket.send_text(message)
+	
+func log_message(message: String) -> void:
+	var time := "[color=#aaaaaa] %s |[/color] " % Time.get_time_string_from_system()
+	print(time + message)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,7 +19,25 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-func _input(event):
-	if event.is_action_pressed("left"):
-		print("left")
+	socket.poll()
+
+	if socket.get_ready_state() == WebSocketPeer.STATE_OPEN:
+		while socket.get_available_packet_count():
+			log_message(socket.get_packet().get_string_from_ascii())
+func _exit_tree() -> void:
+	socket.close()
+	
+func _on_button_pressed() -> void:
+	if socket.connect_to_url(websocket_url) != OK:
+		log_message("Unable to connect.")
+		set_process(false)
+	else:
+		var state = socket.get_ready_state()
+
+		while state == WebSocketPeer.STATE_CONNECTING:
+			state = socket.get_ready_state() 
+			socket.poll()
+		connected = true
+		#initial connection string - sub "lockpick" for selected role
+		send("join|lockpick")
+		print("Connected!")
