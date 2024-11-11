1351e90708c35bd2bc3347366cb71b3adc1a3792
gabe <100031278+vocaz@users.noreply.github.com>
2024-11-11 10:27:29 +0800

    Copying of api to lobbymenu

--- lobby_menu.gd
+++ lobby_menu.gd
@@ -1,11 +1,39 @@
 extends Control
+var websocket_url := "ws://127.0.0.1:9876"
 
+var socket := WebSocketPeer.new()
 
-# Called when the node enters the scene tree for the first time.
+var connected := false
+
+func send(message: String):
+	#socket.put_packet(message.to_utf8_buffer())
+	socket.send_text(message)
+func log_message(message: String) -> void:
+	var time := "[color=#aaaaaa] %s |[/color] " % Time.get_time_string_from_system()
+	print(time + message)
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	socket.poll()
+
+	if socket.get_ready_state() == WebSocketPeer.STATE_OPEN:
+		while socket.get_available_packet_count():
+			log_message(socket.get_packet().get_string_from_ascii())
+
+
+func _on_button_pressed() -> void:
+	if socket.connect_to_url(websocket_url) != OK:
+		log_message("Unable to connect.")
+		set_process(false)
+	else:
+		var state = socket.get_ready_state()
+		while state == WebSocketPeer.STATE_CONNECTING:
+			state = socket.get_ready_state() 
+			socket.poll()
+		connected = true
+		#initial connection string - sub "lockpick" for selected role
+		send("join|lockpick")
+		print("Connected!")
