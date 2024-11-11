97ab73142e8bb544d57d2de17cb358a06a933673
gabe <100031278+vocaz@users.noreply.github.com>
2024-11-11 10:43:55 +0800

    update to new api and connect

--- lobby_menu.gd
+++ lobby_menu.gd
@@ -1,15 +1,19 @@
 extends Control
+
 var websocket_url := "ws://10.202.182.8:9876"
+var api_version = "1.01"
+var client_type = "godot"
+var role = "lockpick"
 
 var socket := WebSocketPeer.new()
-
 var connected := false
 
 func send(message: String):
 	#socket.put_packet(message.to_utf8_buffer())
 	socket.send_text(message)
+
 func log_message(message: String) -> void:
-	var time := "[color=#aaaaaa] %s |[/color] " % Time.get_time_string_from_system()
+	var time := "%s | " % Time.get_time_string_from_system()
 	print(time + message)
 func _ready() -> void:
 	pass # Replace with function body.
@@ -20,6 +24,13 @@ func _process(delta: float) -> void:
 
 	if socket.get_ready_state() == WebSocketPeer.STATE_OPEN:
 		while socket.get_available_packet_count():
+			var server_message = socket.get_parket().get_string_from_ascii()
+			var server_response = JSON.new()
+			if server_response.parse(server_message) == OK:
+				var response = server_response.data
+				if response["type"] == "environment":
+					log_message("Here is the environment around your character: ")
+					print(response["response"])
 			log_message(socket.get_packet().get_string_from_ascii())
 
 
@@ -29,11 +40,13 @@ func _on_button_pressed() -> void:
 		set_process(false)
 	else:
 		var state = socket.get_ready_state()
+		
 		while state == WebSocketPeer.STATE_CONNECTING:
 			state = socket.get_ready_state() 
 			socket.poll()
 		connected = true
 		#initial connection string - sub "lockpick" for selected role
-		send("join|lockpick")
+		var instruction = {"action":"join"}
+		send(instruction)
 		print("Connected!")
 		get_tree().change_scene_to_file("res://Main Menu.tscn")
--- main_menu.gd
+++ main_menu.gd
@@ -1,14 +1,19 @@
 extends Node2D
 
-var socket := WebSocketPeer.new()
+var websocket_url := "ws://10.202.182.8:9876"
+var api_version = "1.01"
+var client_type = "godot"
+var role = "lockpick"
 
+var socket := WebSocketPeer.new()
 var connected := false
+
 func send(message: String):
 	#socket.put_packet(message.to_utf8_buffer())
 	socket.send_text(message)
 	
 func log_message(message: String) -> void:
-	var time := "[color=#aaaaaa] %s |[/color] " % Time.get_time_string_from_system()
+	var time := "%s | " % Time.get_time_string_from_system()
 	print(time + message)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -21,4 +26,11 @@ func _process(delta: float) -> void:
 
 	if socket.get_ready_state() == WebSocketPeer.STATE_OPEN:
 		while socket.get_available_packet_count():
+			var server_message = socket.get_parket().get_string_from_ascii()
+			var server_response = JSON.new()
+			if server_response.parse(server_message) == OK:
+				var response = server_response.data
+				if response["type"] == "environment":
+					log_message("Here is the environment around your character: ")
+					print(response["response"])
 			log_message(socket.get_packet().get_string_from_ascii())
