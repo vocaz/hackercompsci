5c7b44aa17c7a625c6587b18af449bf8f6dac91e
vocaz <100031278+vocaz@users.noreply.github.com>
2024-11-14 12:36:44 +0800

    rand

--- laser.gd
+++ laser.gd
@@ -1,1 +1,12 @@
 extends Node2D
+
+var ascii_letters_and_digits = "abcdefghijklmnopqrstuvwxyz1234567890-=`[]\';,./"
+
+func genSequence(length: int) -> String:
+	var result = ""
+	for i in range(length):
+		result += ascii_letters_and_digits[randi() % ascii_letters_and_digits.length()]
+	return result
+func _ready():
+	for i in range(25):
+		print(genSequence(8))
